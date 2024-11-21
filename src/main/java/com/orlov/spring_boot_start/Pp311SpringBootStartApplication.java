a13276dfb648a605165d608ab2e3a32ffcce19f6
Roman Orlov <main.r.orlov@gmail.com>
2024-11-21 17:02:37 +0300

    Initial commit

---
+++ src/main/java/com/orlov/spring_boot_start/Pp311SpringBootStartApplication.java
@@ -0,0 +1,13 @@
+package com.orlov.spring_boot_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp311SpringBootStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp311SpringBootStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orlov/spring_boot_start/Pp311SpringBootStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orlov.spring_boot_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp311SpringBootStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
